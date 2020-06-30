b9f61f3927ebea4f4ae5806493054cc79351a60f
karan <ksingh209@gmail.com>
2020-06-29 18:23:23 -0700

    Created base html and implemented inheirtance to other pages, updated titles for pages

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'KSINGH',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 19, 2020'
+    },
+    {
+        'author': 'J DOE',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 29, 2020'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
