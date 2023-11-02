a070e4c0a99efdbcdbfe81f2258ad546e54bd7e8
Karan Singh <ksingh209@gmail.com>
2023-11-02 15:38:13 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+// Example JavaScript program with intentional errors
+
+// Error 1: Missing semicolon
+let greeting = "Hello, World!"
+console.log(greeting)
+
+// Error 2: Undefined variable
+console.log(someUndefinedVariable)
+
+// Error 3: Invalid function call
+function sayHello(name) {
+  console.log("Hello, " + name)
+}
+
+sayHello("Alice")
+
+// Error 4: Using const and trying to reassign a value
+const number = 42
+number = 100
+
+// Error 5: Typo in the variable name
+let message = "This is a message"
+console.log(mesage)
